3c46183400f653deca64bc82dff473e729cf90eb
Hori Godai <ess_president@me.com>
2018-02-01 19:31:26 +0900

    bumping version

--- js/lite-editor-emoji-plugin.js
+++ js/lite-editor-emoji-plugin.js
@@ -5,7 +5,7 @@
  * lite-editor-emoji-picker-plugin:
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: steelydylan
- *   version: 1.0.3
+ *   version: 1.1.0
  *
  * jquery:
  *   license: MIT (http://opensource.org/licenses/MIT)

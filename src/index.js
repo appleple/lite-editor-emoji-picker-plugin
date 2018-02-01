36c9cff3a3b9c8da433c98361a274f3d8fcd28a4
Hori Godai <ess_president@me.com>
2018-02-01 12:11:07 +0900

    fix bug when clicked first time

--- src/index.js
+++ src/index.js
@@ -1,24 +1,71 @@
 import EmojiPicker from "rm-emoji-picker";
 
+const defaultCategories = [
+  {
+    title: "People",
+    icon: '<i class="fa fa-smile-o" aria-hidden="true"></i>'
+  },
+  {
+    title: "Nature",
+    icon: '<i class="fa fa-leaf" aria-hidden="true"></i>'
+  },
+  {
+    title: "Foods",
+    icon: '<i class="fa fa-cutlery" aria-hidden="true"></i>'
+  },
+  {
+    title: "Activity",
+    icon: '<i class="fa fa-futbol-o" aria-hidden="true"></i>'
+  },
+  {
+    title: "Places",
+    icon: '<i class="fa fa-globe" aria-hidden="true"></i>'
+  },
+  {
+    title: "Symbols",
+    icon: '<i class="fa fa-lightbulb-o" aria-hidden="true"></i>'
+  },
+  {
+    title: "Flags",
+    icon: '<i class="fa fa-flag-checkered" aria-hidden="true"></i>'
+  }
+];
+
+const fireClick = (node) => {
+  if (document.createEvent) {
+    const evt = document.createEvent('MouseEvents');
+    evt.initEvent('click', true, false);
+    node.dispatchEvent(evt);
+  } else if (document.createEventObject) {
+    node.fireEvent('onclick');
+  } else if (typeof node.onclick == 'function') {
+    node.onclick();
+  }
+}
+
 export default ({
-    label = 'emoji', group
+    label = 'emoji', group, categories = defaultCategories
 }) => {
-  let picker = null; 
+  let picker = null;
   return {
     label,
     group,
     action: 'extra',
+
     onClick: (editor) => {
+      const e = editor.e;
+      const target = e.target;
       if (!picker) {
         const container = document.querySelector(`[data-id="${editor.id}"]`);
         const editable = container.querySelector('[data-selector="lite-editor"]');
         const picker = new EmojiPicker({
           callback: () => {
             editor.onInput();
-          }
+          },
+          categories
         });
-        picker.listenOn(editor.e.target, container, editable);
-        picker.openPicker(editor.e);
+        picker.listenOn(target, container, editable);
+        fireClick(target);
       }
     }
   }
--- test/bundle.js
+++ test/bundle.js
@@ -23901,45 +23901,86 @@ function config (name) {
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],100:[function(require,module,exports){
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _rmEmojiPicker = require('rm-emoji-picker');
+var _rmEmojiPicker = require("rm-emoji-picker");
 
 var _rmEmojiPicker2 = _interopRequireDefault(_rmEmojiPicker);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var defaultCategories = [{
+  title: "People",
+  icon: '<i class="fa fa-smile-o" aria-hidden="true"></i>'
+}, {
+  title: "Nature",
+  icon: '<i class="fa fa-leaf" aria-hidden="true"></i>'
+}, {
+  title: "Foods",
+  icon: '<i class="fa fa-cutlery" aria-hidden="true"></i>'
+}, {
+  title: "Activity",
+  icon: '<i class="fa fa-futbol-o" aria-hidden="true"></i>'
+}, {
+  title: "Places",
+  icon: '<i class="fa fa-globe" aria-hidden="true"></i>'
+}, {
+  title: "Symbols",
+  icon: '<i class="fa fa-lightbulb-o" aria-hidden="true"></i>'
+}, {
+  title: "Flags",
+  icon: '<i class="fa fa-flag-checkered" aria-hidden="true"></i>'
+}];
+
+var fireClick = function fireClick(node) {
+  if (document.createEvent) {
+    var evt = document.createEvent('MouseEvents');
+    evt.initEvent('click', true, false);
+    node.dispatchEvent(evt);
+  } else if (document.createEventObject) {
+    node.fireEvent('onclick');
+  } else if (typeof node.onclick == 'function') {
+    node.onclick();
+  }
+};
+
 exports.default = function (_ref) {
   var _ref$label = _ref.label,
       label = _ref$label === undefined ? 'emoji' : _ref$label,
-      group = _ref.group;
+      group = _ref.group,
+      _ref$categories = _ref.categories,
+      categories = _ref$categories === undefined ? defaultCategories : _ref$categories;
 
   var picker = null;
   return {
     label: label,
     group: group,
     action: 'extra',
+
     onClick: function onClick(editor) {
+      var e = editor.e;
+      var target = e.target;
       if (!picker) {
-        var container = document.querySelector('[data-id="' + editor.id + '"]');
+        var container = document.querySelector("[data-id=\"" + editor.id + "\"]");
         var editable = container.querySelector('[data-selector="lite-editor"]');
         var _picker = new _rmEmojiPicker2.default({
           callback: function callback() {
             editor.onInput();
-          }
+          },
+          categories: categories
         });
-        _picker.listenOn(editor.e.target, container, editable);
-        _picker.openPicker(editor.e);
+        _picker.listenOn(target, container, editable);
+        fireClick(target);
       }
     }
   };
 };
 
-module.exports = exports['default'];
+module.exports = exports["default"];
 
 },{"rm-emoji-picker":94}],101:[function(require,module,exports){
 'use strict';
